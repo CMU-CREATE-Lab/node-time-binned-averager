a5c98f11fdaecef4c7ddb3afee59ecb75c74a31d
Chris Bartley <chris@chrisbartley.com>
2020-10-21 14:44:12 -0400

    New API, much better suited to averaging exported JSON records from the datastore.

--- index.js
+++ index.js
@@ -7,41 +7,44 @@ const cloneObject = function(obj) {
    return JSON.parse(JSON.stringify(obj));
 };
 
+// Computes the average of the given array.  Returns null if the array is empty.
 // Props to https://stackoverflow.com/a/10624256/703200
 const computeAverage = function(arr) {
+   if (!Array.isArray(arr) || arr.length <= 0) {
+      return null;
+   }
+
    const sum = arr.reduce((a, b) => a + b, 0);
-   return (sum / arr.length) || 0;
+   return (sum / arr.length);
 };
 
 class TimeBinnedAverager extends EventEmitter {
-   constructor(binSizeSeconds, channelNames) {
+   constructor(binSizeSeconds, numChannels) {
       super();
       if (!TypeUtils.isPositiveInt(binSizeSeconds)) {
          throw new TypeError("binSizeSeconds must be a positive integer");
       }
 
-      // make sure channelNames is a non-empty array
-      if (!Array.isArray(channelNames) || channelNames.length < 1) {
-         throw new TypeError("channelNames must be a non-empty array");
+      if (!TypeUtils.isPositiveInt(numChannels)) {
+         throw new TypeError("numChannels must be a positive integer");
       }
 
       this._binSizeSeconds = parseInt(binSizeSeconds);
+      this._numChannels = parseInt(numChannels);
       this._binBounds = null;
       this._timestamps = [];
-      this._channelValues = {};
+      this._channelValues = [];
 
-      // make sure channel names are strings
-      const self = this;
-      channelNames.forEach(function(channelName) {
-         if (!TypeUtils.isNonEmptyString(channelName)) {
-            throw new TypeError("Channel [" + channelName + "] is not a string.  All channel names must be strings.");
-         }
+      // initialize this._channelValues with empty arrays
+      for (let i = 0; i < this._numChannels; i++) {
+         this._channelValues.push([]);
+      }
 
-         self._channelValues[channelName] = [];
-      });
-
-      this._channelNames = new Set(channelNames);
+      // save a copy of the initialized this._channelValues for quick and easy reset when creating a new bin
       this._emptyChannelValues = cloneObject(this._channelValues);
+
+      // Add one for the timestamp, which is assumed to be the first value of the record
+      this._expectedRecordLength = 1 + this._numChannels;
    }
 
    _getBinBoundsForTime(unixTimeSeconds) {
@@ -54,32 +57,27 @@ class TimeBinnedAverager extends EventEmitter {
       return (this._binBounds.start <= unixTimeSeconds && unixTimeSeconds < this._binBounds.end);
    }
 
-   appendSample(unixTimeSeconds, channelValues) {
+   appendSample(record) {
       const self = this;
       let avgSample = null;
 
+      if (!Array.isArray(record) || record.length !== self._expectedRecordLength) {
+         throw new TypeError("Record must be an array of length " + self._expectedRecordLength);
+      }
+
+      // timestamp must be the first element
+      const unixTimeSeconds = record[0];
+
       if (!TypeUtils.isNumeric(unixTimeSeconds)) {
-         throw new TypeError("unixTimeSeconds must be numeric")
+         throw new TypeError("The first element of the record (timestamp in UNIX time seconds) must be numeric");
       }
 
       // make sure the timestamp is strictly increasing
       if (self._timestamps.length > 0) {
          if (unixTimeSeconds <= self._timestamps[self._timestamps.length - 1]) {
-            throw new Error("Timestamps must be added in strictly increasing order")
+            throw new Error("Records must be added with timestamps in strictly increasing order")
          }
       }
-
-      // make sure the channelValues object contains all the expected channels and the channel values are all numeric
-      this._channelNames.forEach(function(channelName) {
-         if (channelName in channelValues) {
-            if (!TypeUtils.isNumeric(channelValues[channelName])) {
-               throw new TypeError("Value for channel '" + channelName + "' must be numeric")
-            }
-         }
-         else {
-            throw new Error("Invalid channelValues, missing expected channel: " + channelName);
-         }
-      });
 
       // If we don't have a current time bin yet, then just compute beginning and ending times for the time bin
       if (self._binBounds === null) {
@@ -89,22 +87,27 @@ class TimeBinnedAverager extends EventEmitter {
          // Otherwise, if this new sample is NOT in the current bin, then it's time to compute the average for the
          // timestamps and channel values in the current bin, emit an event, and create a new bin
          if (!self._isInBin(unixTimeSeconds)) {
-            avgSample = this.computeAverageSample();
+            avgSample = self.computeAverageSample();
 
             // create new bounds and reset
             self._binBounds = self._getBinBoundsForTime(unixTimeSeconds);
             self._timestamps = [];
-            self._channelValues = cloneObject(this._emptyChannelValues);
+            self._channelValues = cloneObject(self._emptyChannelValues);
 
             self.emit("averageSample", avgSample);
          }
       }
 
-      // now simply add the channelValues to the bin
+      // record this timestamp for the bin
       self._timestamps.push(parseFloat(unixTimeSeconds));
-      self._channelNames.forEach(function(channelName) {
-         self._channelValues[channelName].push(parseFloat(channelValues[channelName]));
-      });
+
+      // add channel values to their appropriate array for this bin, skipping any values which are not numeric
+      for (let i = 0; i < self._channelValues.length; i++) {
+         const newValue = record[i + 1];    // add one to the index to account for the timestamp
+         if (TypeUtils.isNumeric(newValue)) {
+            self._channelValues[i].push(parseFloat(newValue));
+         }
+      }
 
       return avgSample;
    }
@@ -114,17 +117,14 @@ class TimeBinnedAverager extends EventEmitter {
       const self = this;
 
       if (self._binBounds !== null) {
-         const avgTimestamp = computeAverage(self._timestamps);
-         const avgValues = {};
+         const averageSample = [computeAverage(self._timestamps)];
 
-         self._channelNames.forEach(function(channel) {
-            avgValues[channel] = computeAverage(self._channelValues[channel]);
-         });
+         // compute the average of all the values within each channel
+         for (let i = 0; i < self._channelValues.length; i++) {
+            averageSample[i + 1] = computeAverage(self._channelValues[i]);
+         }
 
-         return {
-            timestamp : avgTimestamp,
-            values : avgValues
-         };
+         return averageSample;
       }
 
       return null;
--- test/index.js
+++ test/index.js
@@ -8,146 +8,170 @@ describe('constructor', function() {
    it('should throw a TypeError if binSizeSeconds is undefined', function() {
       expect(function() {new TimeBinnedAverager();}).to.throw(TypeError);
       let x;
-      expect(function() {new TimeBinnedAverager(x, ['foo']);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(x, 1);}).to.throw(TypeError);
    });
    it('should throw a TypeError if binSizeSeconds is null', function() {
       expect(function() {new TimeBinnedAverager(null);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(null, ['foo']);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(null, 1);}).to.throw(TypeError);
    });
    it('should throw a TypeError if binSizeSeconds is negative', function() {
       expect(function() {new TimeBinnedAverager(-1);}).to.throw(TypeError);
       expect(function() {new TimeBinnedAverager(-42.42);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(-1, ['foo']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(-42.42, ['foo']);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(-1, 1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(-42.42, 1);}).to.throw(TypeError);
    });
    it('should throw a TypeError if binSizeSeconds is zero', function() {
       expect(function() {new TimeBinnedAverager(0);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(0, ['foo']);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(0, 1);}).to.throw(TypeError);
    });
    it('should throw a TypeError if binSizeSeconds is numeric and positive, but not an integer', function() {
       expect(function() {new TimeBinnedAverager(0.04);}).to.throw(TypeError);
       expect(function() {new TimeBinnedAverager(42.42);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(0.04, ['foo']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(42.42, ['foo']);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(0.04, 1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(42.42, 1);}).to.throw(TypeError);
    });
    it('should throw a TypeError if binSizeSeconds is not an integer', function() {
-      expect(function() {new TimeBinnedAverager(['hi']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager({});}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager({ foo : 'bar' });}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager("hello world");}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(new Set());}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(new Date());}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(['hi'], 1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager({}, 1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager({ foo : 'bar' }, 1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager("hello world", 1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(new Set(), 1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(new Date(), 1);}).to.throw(TypeError);
    });
-   it('should throw a TypeError if channelNames is not an Array', function() {
+
+   it('should throw a TypeError if numChannels is undefined', function() {
       expect(function() {new TimeBinnedAverager(30);}).to.throw(TypeError);
+      let x;
+      expect(function() {new TimeBinnedAverager(30, x);}).to.throw(TypeError);
+   });
+   it('should throw a TypeError if numChannels is null', function() {
       expect(function() {new TimeBinnedAverager(30, null);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, 4);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, 4.2);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, {});}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, { foo : 'bar' });}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, "hello world");}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, new Set());}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, new Date());}).to.throw(TypeError);
    });
-   it('should throw a TypeError if channelNames is an empty Array', function() {
-      expect(function() {new TimeBinnedAverager(30, []);}).to.throw(TypeError);
+   it('should throw a TypeError if numChannels is negative', function() {
+      expect(function() {new TimeBinnedAverager(30, -1);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(30, -42.42);}).to.throw(TypeError);
    });
-   it('should throw a TypeError if channelNames contains one or more items that are not Strings', function() {
-      expect(function() {new TimeBinnedAverager(30, ['c1', 2]);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, [1, 'c1']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, ['c1', {}, 'c2']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, ['c1', [], 'c2']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, ['c1', null, 'c2']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, ['c1', new Date(), 'c2']);}).to.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, ['c1', 42.42, 'c2']);}).to.throw(TypeError);
+   it('should throw a TypeError if numChannels is zero', function() {
+      expect(function() {new TimeBinnedAverager(30, 0);}).to.throw(TypeError);
    });
-   it('should succeed if binSizeSeconds is a positive integer and channelNames is a non-empty Array containing one or more Strings', function() {
-      expect(function() {new TimeBinnedAverager(30, ['c1']);}).to.not.throw(TypeError);
-      expect(function() {new TimeBinnedAverager(30, ['c1', 'c2']);}).to.not.throw(TypeError);
+   it('should throw a TypeError if numChannels is numeric and positive, but not an integer', function() {
+      expect(function() {new TimeBinnedAverager(30, 0.04);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(30, 42.42);}).to.throw(TypeError);
+   });
+   it('should throw a TypeError if numChannels is not an integer', function() {
+      expect(function() {new TimeBinnedAverager(15, ['hi']);}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(15, {});}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(15, { foo : 'bar' });}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(15, "hello world");}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(15, new Set());}).to.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(15, new Date());}).to.throw(TypeError);
+   });
+
+   it('should succeed if binSizeSeconds and numChannels are both positive integers', function() {
+      expect(function() {new TimeBinnedAverager(30, 1);}).to.not.throw(TypeError);
+      expect(function() {new TimeBinnedAverager("30", 37);}).to.not.throw(TypeError);
+      expect(function() {new TimeBinnedAverager(30, "42");}).to.not.throw(TypeError);
    });
 });
 
 describe('appendSample()', function() {
    it('should throw a TypeError if unixTimeSeconds is not numeric', function() {
-      const averager = new TimeBinnedAverager(30, ['voltage']);
+      const averager = new TimeBinnedAverager(30, 1);
       let x;
-      expect(function() {averager.appendSample(null, { voltage : 15 })}).to.throw(TypeError);
-      expect(function() {averager.appendSample(x, { voltage : 15 })}).to.throw(TypeError);
-      expect(function() {averager.appendSample("", { voltage : 15 })}).to.throw(TypeError);
-      expect(function() {averager.appendSample("hello!", { voltage : 15 })}).to.throw(TypeError);
-      expect(function() {averager.appendSample({}, { voltage : 15 })}).to.throw(TypeError);
-      expect(function() {averager.appendSample([], { voltage : 15 })}).to.throw(TypeError);
-      expect(function() {averager.appendSample(new Date(), { voltage : 15 })}).to.throw(TypeError);
+      expect(function() {averager.appendSample([null, 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample([, 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample([x, 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample(["", 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample(["hello!", 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample([{}, 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample([[], 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample([new Date(), 15])}).to.throw(TypeError);
+      expect(function() {averager.appendSample([new Set(), 15])}).to.throw(TypeError);
    });
 
    it('should throw an Error if samples are not added with strictly increasing timestamps', function() {
-      const averager = new TimeBinnedAverager(30, ['voltage']);
-      expect(averager.appendSample(0, { voltage : 10 })).to.be.null;
-      expect(averager.appendSample(1, { voltage : 15 })).to.be.null;
-      expect(function() {averager.appendSample(1, { voltage : 15 })}).to.throw(Error);
-      expect(averager.appendSample(2, { voltage : 15 })).to.be.null;
-      expect(function() {averager.appendSample(1.5, { voltage : 15 })}).to.throw(Error);
-      expect(function() {averager.appendSample(1.9999, { voltage : 15 })}).to.throw(Error);
-      expect(averager.appendSample(2.000001, { voltage : 15 })).to.be.null;
+      const averager = new TimeBinnedAverager(30, 1);
+      expect(averager.appendSample([0, 10])).to.be.null;
+      expect(averager.appendSample([1, 15])).to.be.null;
+      expect(function() {averager.appendSample([1, 15])}).to.throw(Error);
+      expect(averager.appendSample([2, 15])).to.be.null;
+      expect(function() {averager.appendSample([1.5, 15])}).to.throw(Error);
+      expect(function() {averager.appendSample([1.9999, 15])}).to.throw(Error);
+      expect(averager.appendSample([2.000001, 15])).to.be.null;
+      expect(averager.appendSample([30, 15])).to.deep.equal([1.25000025, 13.75]);
+      expect(function() {averager.appendSample([29.9999, 15])}).to.throw(Error);
    });
 
    it('should throw an Error if a sample is added with missing expected channels', function() {
-      const averager = new TimeBinnedAverager(30, ['c1', 'c2']);
-      expect(function() {averager.appendSample(1, { c1 : 1 })}).to.throw(Error);
-      expect(function() {averager.appendSample(2, { c2 : 2 })}).to.throw(Error);
-      expect(function() {averager.appendSample(3, { c3 : 3 })}).to.throw(Error);
+      const averager = new TimeBinnedAverager(30, 3);
+      expect(function() {averager.appendSample([1])}).to.throw(Error);
+      expect(function() {averager.appendSample([2, 10])}).to.throw(Error);
+      expect(function() {averager.appendSample([3, 10, 20])}).to.throw(Error);
+      expect(averager.appendSample([3, 10, 20, 30])).to.be.null;
    });
 
-   it('should throw a TypeError if a sample is added with non-numeric channel values', function() {
-      const averager = new TimeBinnedAverager(30, ['c1']);
-      expect(averager.appendSample(1, { c1 : 1 })).to.be.null;
-      expect(averager.appendSample(2, { c1 : "2" })).to.be.null;
-      expect(averager.appendSample(3, { c1 : "3.0" })).to.be.null;
-      expect(function() {averager.appendSample(4, { c1 : "cat" })}).to.throw(TypeError);
-      expect(function() {averager.appendSample(4, { c1 : [] })}).to.throw(TypeError);
-      expect(function() {averager.appendSample(4, { c1 : {} })}).to.throw(TypeError);
-      expect(function() {averager.appendSample(4, { c1 : new Date() })}).to.throw(TypeError);
-      expect(function() {averager.appendSample(4, { c1 : null })}).to.throw(TypeError);
+   it('should skip non-numeric channel values', function() {
+      const averager = new TimeBinnedAverager(30, 4);
+      let x;
+      expect(averager.appendSample([1, 10, 20, 30, 100])).to.be.null;
+      expect(averager.appendSample([2, 11, 21, 31, null])).to.be.null;
+      expect(averager.appendSample([3, 12, 22, 32, null])).to.be.null;
+      expect(averager.appendSample([4, 13, null, 33, null])).to.be.null;
+      expect(averager.computeAverageSample()).to.deep.equal([2.5, 11.5, 21, 31.5, 100]);
+      expect(averager.appendSample([5, , "foo", x, 100])).to.be.null;
+      expect(averager.appendSample([6, , "10", x, null])).to.be.null;
+      expect(averager.appendSample([7, , [], x, null])).to.be.null;
+      expect(averager.appendSample([8, , new Date(), x, null])).to.be.null;
+      expect(averager.computeAverageSample()).to.deep.equal([4.5, 11.5, 18.25, 31.5, 100]);
    });
 
    it('should return null as long as samples are added to the same time bin, but return the average sample when new bins are started', function() {
-      const averager = new TimeBinnedAverager(30, ['voltage']);
-      expect(averager.appendSample(0, { voltage : 10 })).to.be.null;
-      expect(averager.appendSample(1, { voltage : 15 })).to.be.null;
-      expect(averager.appendSample(2, { voltage : 17 })).to.be.null;
-      expect(averager.appendSample(30, { voltage : 100 })).to.deep.equal({ timestamp : 1, values : { voltage : 14 } });
-      expect(averager.computeAverageSample()).to.deep.equal({ timestamp : 30, values : { voltage : 100 } });
-      expect(averager.appendSample(65, { voltage : 42.000343 })).to.deep.equal({
-                                                                                  timestamp : 30,
-                                                                                  values : { voltage : 100 }
-                                                                               });
-      expect(averager.appendSample(65.0001, { voltage : 74 })).to.be.null;
-      expect(averager.appendSample(65.0002, { voltage : 19.23823 })).to.be.null;
-      expect(averager.appendSample(89.9999, { voltage : 34233 })).to.be.null;
-      expect(averager.computeAverageSample()).to.deep.equal({
-                                                               timestamp : 71.25005,
-                                                               values : { voltage : 8592.05964325 }
-                                                            });
-      expect(averager.appendSample(90, { voltage : 1 })).to.deep.equal({
-                                                                          timestamp : 71.25005,
-                                                                          values : { voltage : 8592.05964325 }
-                                                                       });
+      const averager = new TimeBinnedAverager(30, 1);
+      expect(averager.appendSample([0, 10])).to.be.null;
+      expect(averager.appendSample([1, 15])).to.be.null;
+      expect(averager.appendSample([2, 17])).to.be.null;
+      expect(averager.appendSample([30, 100])).to.deep.equal([1, 14]);
+      expect(averager.computeAverageSample()).to.deep.equal([30, 100]);
+      expect(averager.appendSample([65, 42.000343])).to.deep.equal([30, 100]);
+      expect(averager.appendSample([65.0001, 74])).to.be.null;
+      expect(averager.appendSample([65.0002, 19.23823])).to.be.null;
+      expect(averager.appendSample([89.9999, 34233])).to.be.null;
+      expect(averager.computeAverageSample()).to.deep.equal([71.25005, 8592.05964325]);
+      expect(averager.appendSample([90, 1])).to.deep.equal([71.25005, 8592.05964325]);
+      expect(averager.computeAverageSample()).to.deep.equal([90, 1]);
+
+      let x;
+      const averager2 = new TimeBinnedAverager(30, 3);
+      expect(averager2.appendSample([1, 10, 2, 42])).to.be.null;
+      expect(averager2.appendSample([8, 11, 4, null])).to.be.null;
+      expect(averager2.appendSample([15, 12, 8, null])).to.be.null;
+      expect(averager2.appendSample([22, 13, 16, null])).to.be.null;
+      expect(averager2.appendSample([29, 14, 32, null])).to.be.null;
+      expect(averager2.appendSample([36, 15, 64, null])).to.deep.equal([15, 12, 12.4, 42]);
+      expect(averager2.appendSample([43, 16, 128, null])).to.be.null;
+      expect(averager2.appendSample([50, 17, 256, null])).to.be.null;
+      expect(averager2.appendSample([57, 18, 512, null])).to.be.null;
+      expect(averager2.appendSample([64, 19, 1024, x])).to.deep.equal([46.5, 16.5, 240, null]);
+      expect(averager2.appendSample([71, 20, 2048, x])).to.be.null;
+      expect(averager2.appendSample([78, 21, 4096, x])).to.be.null;
+      expect(averager2.appendSample([85, 22, 8192, x])).to.be.null;
+      expect(averager2.appendSample([92, 23, 16384, x])).to.deep.equal([74.5, 20.5, 3840, null]);
    });
 })
 
 describe('computeAverageSample()', function() {
    it('should return null if no samples have been added', function() {
-      const averager = new TimeBinnedAverager(30, ['voltage']);
+      const averager = new TimeBinnedAverager(30, 1);
       expect(averager.computeAverageSample()).to.be.null;
    });
 
    it('should return the correct averaged timestamp and value(s) as samples are appended', function() {
-      const averager = new TimeBinnedAverager(30, ['voltage']);
-      expect(averager.appendSample(0, { voltage : 10 })).to.be.null;
-      expect(averager.computeAverageSample()).to.deep.equal({ timestamp : 0, values : { voltage : 10 } });
-      expect(averager.appendSample(1, { voltage : 15 })).to.be.null;
-      expect(averager.computeAverageSample()).to.deep.equal({ timestamp : .5, values : { voltage : 12.5 } });
-      expect(averager.appendSample(2, { voltage : 17 })).to.be.null;
-      expect(averager.computeAverageSample()).to.deep.equal({ timestamp : 1, values : { voltage : 14 } });
+      const averager = new TimeBinnedAverager(30, 1);
+      expect(averager.appendSample([0, 10])).to.be.null;
+      expect(averager.computeAverageSample()).to.deep.equal([0, 10]);
+      expect(averager.appendSample([1, 15])).to.be.null;
+      expect(averager.computeAverageSample()).to.deep.equal([.5, 12.5]);
+      expect(averager.appendSample([2, 17])).to.be.null;
+      expect(averager.computeAverageSample()).to.deep.equal([1, 14]);
    });
 });
